5f81bcb3872da1ae48f9d8b10b6ea4cb0088d935
guanhg <guanhongguang@kanjian.com>
2018-10-26 20:07:38 +0800

    nlp

--- nlp/segment.py
+++ nlp/segment.py
@@ -10,65 +10,6 @@ import numpy as np
 import re
 
 
-def load_data(path):
-    data = open(path).read().rstrip('\n')
-    # 按标点符号和换行符分隔
-    data = re.split('[，。！？、\n]', data)
-    print('共有数据 %d 条' % len(data))
-    print('平均长度：', np.mean([len(d.replace(' ', '')) for d in data]))
-
-    # 准备数据
-    X_data = []
-    y_data = []
-
-    for sentence in data:
-        sentence = sentence.split(' ')
-        X = []
-        y = []
-
-        try:
-            for s in sentence:
-                s = s.strip()
-                # 跳过空字符
-                if len(s) == 0:
-                    continue
-                # s
-                elif len(s) == 1:
-                    X.append(char2id[s])
-                    y.append(tags['s'])
-                elif len(s) > 1:
-                    # b
-                    X.append(char2id[s[0]])
-                    y.append(tags['b'])
-                    # m
-                    for i in range(1, len(s) - 1):
-                        X.append(char2id[s[i]])
-                        y.append(tags['m'])
-                    # e
-                    X.append(char2id[s[-1]])
-                    y.append(tags['e'])
-
-            # 统一长度
-            if len(X) > maxlen:
-                X = X[:maxlen]
-                y = y[:maxlen]
-            else:
-                for i in range(maxlen - len(X)):
-                    X.append(0)
-                    y.append(tags['x'])
-        except:
-            continue
-        else:
-            if len(X) > 0:
-                X_data.append(X)
-                y_data.append(y)
-
-    X_data = np.array(X_data)
-    y_data = np_utils.to_categorical(y_data, 5)
-
-    return X_data, y_data
-
-
 def bilstm_build(maxlen, vocab_size, emb_size, lstm_unit, tag_cnt=5, drop_rate=0.6):
     """Ref: https://juejin.im/post/5ba2ff3a5188255c672ea8e5"""
     # BiLSTM
@@ -139,49 +80,122 @@ def bilstm_cnn_crf_build(maxlen, vocab_size, emb_size, lstm_unit, tag_cnt=5, dro
     return model
 
 
-def train_and_save(model, file_name):
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs)
-    model.save('../model/segment/'+file_name)
-    print('Train: ', model.evaluate(X_train, y_train, batch_size=batch_size))
-    print('Test: ', model.evaluate(X_test, y_test, batch_size=batch_size))
+class SegmentSetting:
+
+    def __init__(self, model_name):
+        vocab = self.vocab_data()
+        # 5167 个字
+        self.vocabSize = len(vocab) + 1
+        # 映射
+        self.char2id = {c: i + 1 for i, c in enumerate(vocab)}
+        self.id2char = {i + 1: c for i, c in enumerate(vocab)}
+        self.tags = {'s': 0, 'b': 1, 'm': 2, 'e': 3, 'x': 4}
+        # 定义参数
+        self.embedding_size = 128
+        self.maxlen = 32  # 长于32则截断，短于32则填充0
+        self.hidden_size = 64
+        self.batch_size = 64
+        self.epochs = 2
+
+        self.models = dict(
+            bilstm=bilstm_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.6),
+            bilstm_crf=bilstm_crf_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.6),
+            bilstm_cnn_crf=bilstm_cnn_crf_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.15),
+        )
+
+        self.model = self.models[model_name]
+
+        self.bilstm_model=bilstm_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.6),
+        self.bilstm_crf_model=bilstm_crf_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.6),
+        self.bilstm_cnn_crf_model=bilstm_cnn_crf_build(self.maxlen, self.vocabSize, self.embedding_size, self.hidden_size, drop_rate=0.15),
+
+    def load_model(self, h5_path):
+        self.model.load_weights(h5_path)
+        return self.model
+
+    def train_and_save(self, file_name):
+        x_train, y_train = self.load_data('../data/msr/msr_training.utf8')
+        x_test, y_test = self.load_data('../data/msr/msr_test_gold.utf8')
+        self.model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.epochs)
+        self.model.save('../model/segment/' + file_name)
+        print('Train: ', self.model.evaluate(x_train, y_train, batch_size=self.batch_size))
+        print('Test: ', self.model.evaluate(x_test, y_test, batch_size=self.batch_size))
+
+    def load_data(self, path):
+        data = open(path).read().rstrip('\n')
+        # 按标点符号和换行符分隔
+        data = re.split('[，。！？、\n]', data)
+        print('共有数据 %d 条' % len(data))
+        print('平均长度：', np.mean([len(d.replace(' ', '')) for d in data]))
+
+        # 准备数据
+        X_data = []
+        y_data = []
+
+        for sentence in data:
+            sentence = sentence.split(' ')
+            X = []
+            y = []
+
+            try:
+                for s in sentence:
+                    s = s.strip()
+                    # 跳过空字符
+                    if len(s) == 0:
+                        continue
+                    # s
+                    elif len(s) == 1:
+                        X.append(self.char2id[s])
+                        y.append(self.tags['s'])
+                    elif len(s) > 1:
+                        # b
+                        X.append(self.char2id[s[0]])
+                        y.append(self.tags['b'])
+                        # m
+                        for i in range(1, len(s) - 1):
+                            X.append(self.char2id[s[i]])
+                            y.append(self.tags['m'])
+                        # e
+                        X.append(self.char2id[s[-1]])
+                        y.append(self.tags['e'])
+
+                # 统一长度
+                if len(X) > self.maxlen:
+                    X = X[:self.maxlen]
+                    y = y[:self.maxlen]
+                else:
+                    for i in range(self.maxlen - len(X)):
+                        X.append(0)
+                        y.append(self.tags['x'])
+            except:
+                continue
+            else:
+                if len(X) > 0:
+                    X_data.append(X)
+                    y_data.append(y)
+
+        X_data = np.array(X_data)
+        y_data = np_utils.to_categorical(y_data, 5)
+
+        return X_data, y_data
+
+    @classmethod
+    def vocab_data(cls):
+        vocab = open('../data/msr/msr_training_words.utf8').read().rstrip('\n').split('\n')
+        vocab = list(''.join(vocab))
+        stat = {}
+        for v in vocab:
+            stat[v] = stat.get(v, 0) + 1
+        stat = sorted(stat.items(), key=lambda x: x[1], reverse=True)
+        vocab = [s[0] for s in stat]
+        return vocab
 
 
 if __name__ == '__main__':
-    # 读取字典
-    vocab = open('../data/msr/msr_training_words.utf8').read().rstrip('\n').split('\n')
-    vocab = list(''.join(vocab))
-    stat = {}
-    for v in vocab:
-        stat[v] = stat.get(v, 0) + 1
-    stat = sorted(stat.items(), key=lambda x: x[1], reverse=True)
-    vocab = [s[0] for s in stat]
-    # 5167 个字
-    vocabSize = len(vocab) + 1
-    print(len(vocab))
-    # 映射
-    char2id = {c: i + 1 for i, c in enumerate(vocab)}
-    id2char = {i + 1: c for i, c in enumerate(vocab)}
-    tags = {'s': 0, 'b': 1, 'm': 2, 'e': 3, 'x': 4}
 
-    # 定义参数
-    embedding_size = 128
-    maxlen = 32  # 长于32则截断，短于32则填充0
-    hidden_size = 64
-    batch_size = 64
-    epochs = 2
+    model = SegmentSetting('bilstm')
 
-    X_train, y_train = load_data('../data/msr/msr_training.utf8')
-    X_test, y_test = load_data('../data/msr/msr_test_gold.utf8')
-    print('X_train size:', X_train.shape)
-    print('y_train size:', y_train.shape)
-    print('X_test size:', X_test.shape)
-    print('y_test size:', y_test.shape)
-
-    bilstm_model = bilstm_build(maxlen, vocabSize, embedding_size, hidden_size, drop_rate=0.6)
-    bilstm_crf_model = bilstm_crf_build(maxlen, vocabSize, embedding_size, hidden_size, drop_rate=0.6)
-    bilstm_cnn_crf_model = bilstm_cnn_crf_build(maxlen, vocabSize, embedding_size, hidden_size, drop_rate=0.15)
-
-    train_and_save(bilstm_crf_model, 'bilstm_crf.h5')
+    model.train_and_save('bilstm.h5')
 
 
 
--- test/segment_crf.py
+++ test/segment_crf.py
@@ -33,64 +33,6 @@ seg = SegmentSetting('bilstm_cnn_crf')
 model = seg.load_model('../model/segment/bilstm_cnn_crf2.h5')
 
 
-def viterbi(nodes):
-    trans = {'be': 0.5, 'bm': 0.5, 'eb': 0.5, 'es': 0.5, 'me': 0.5, 'mm': 0.5, 'sb': 0.5, 'ss': 0.5}
-    paths = {'b': nodes[0]['b'], 's': nodes[0]['s']}
-    for l in range(1, len(nodes)):
-        paths_ = paths.copy()
-        paths = {}
-        for i in nodes[l].keys():
-            nows = {}
-            for j in paths_.keys():
-                if j[-1] + i in trans.keys():
-                    nows[j + i] = paths_[j] + nodes[l][i] + trans[j[-1] + i]
-            nows = sorted(nows.items(), key=lambda x: x[1], reverse=True)
-            paths[nows[0][0]] = nows[0][1]
-
-    paths = sorted(paths.items(), key=lambda x: x[1], reverse=True)
-    return paths[0][0]
-
-
-def cut_words(data):
-    data = re.split('[，。！？、\n]', data)
-    sens = []
-    Xs = []
-    for sentence in data:
-        sen = []
-        X = []
-        sentence = list(sentence)
-        for s in sentence:
-            s = s.strip()
-            if not s == '' and s in char2id:
-                sen.append(s)
-                X.append(char2id[s])
-        if len(X) > maxlen:
-            sen = sen[:maxlen]
-            X = X[:maxlen]
-        else:
-            for i in range(maxlen - len(X)):
-                X.append(0)
-
-        if len(sen) > 0:
-            Xs.append(X)
-            sens.append(sen)
-
-    Xs = np.array(Xs)
-    ys = model.predict(Xs)
-
-    results = ''
-    for i in range(ys.shape[0]):
-        nodes = [dict(zip(['s', 'b', 'm', 'e'], d[:4])) for d in ys[i]]
-        ts = viterbi(nodes)
-        for x in range(len(sens[i])):
-            if ts[x] in ['s', 'e']:
-                results += sens[i][x] + '/'
-            else:
-                results += sens[i][x]
-
-    return results[:-1]
-
-
 def cut_words_crf(data):
     data = re.split('[，。！？、\n]', data)
     sens = []
